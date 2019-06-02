59f300de6170c1954bfdd00b3395561782cef0e9
aiokr <hiaiokr@gmail.com>
2019-06-02 23:40:45 +0800

    update prev & next

--- themes/hexo-theme-Daily/source/js/app.js
+++ themes/hexo-theme-Daily/source/js/app.js
@@ -22,20 +22,4 @@ var appDaily = {
 
     return false;
   },
-  bindToggleButton: function() {
-    var btn = document.querySelector('.menu-toggle');
-    var nav = document.querySelector('.navbar');
-
-    btn.addEventListener('click', function() {
-      var c = nav.getAttribute('class') || '';
-
-      if (c.indexOf('show-force') !== -1) {
-        nav.setAttribute('class', c.replace(/show-force/, '').trim());
-      } else {
-        nav.setAttribute('class', (c + ' show-force').trim());
-      }
-    });
-  }
 };
-
-appDaily.bindToggleButton();
