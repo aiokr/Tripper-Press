90225bbaaa23f0690cf6af545e834715e1c51d3c
aiokr <hiaiokr@gmail.com>
2020-02-08 20:07:44 +0800

    一些胡乱的小修复 fancybox不知道为什么被我删了 加了一个阅读进度条 把封面图从nav里面挪出来了，方便以后上pjax 把daily的css删掉了

--- themes/hexo-theme-type/source/js/app.js
+++ themes/hexo-theme-type/source/js/app.js
@@ -66,3 +66,42 @@ $(".itp-post-toc ol").replaceWith(function () {
 });
 
 */
+
+$("img").not('footer img, .drawer-img, .mdui-card-header-avatar').each(function () {
+  // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
+  var element = document.createElement("a");
+  $(element).attr("data-fancybox", "gallery");
+  $(element).attr("href", $(this).attr("src"));
+  $(this).wrap(element);
+})
+
+$('[data-fancybox="gallery"]').fancybox({
+  buttons: [
+    "close",
+  ],
+  baseTpl:
+    '<div class="fancybox-container" role="dialog" tabindex="-1">' +
+    '<div class="fancybox-bg"></div>' +
+    '<div class="fancybox-inner">' +
+    '<div class="fancybox-infobar">' +
+    '<span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span>' +
+    '</div>' +
+    '<div class="fancybox-toolbar">{{buttons}}</div>' +
+    '<div class="fancybox-navigation">{{arrows}}</div>' +
+    '<div class="fancybox-stage"></div>' +
+    '</div>' +
+    '</div>',
+}); 
+
+document.addEventListener('DOMContentLoaded', function () {
+  var winHeight = window.innerHeight,
+        docHeight = document.documentElement.scrollHeight,
+        progressBar = document.querySelector('#content_progress');
+  progressBar.max = docHeight - winHeight;
+  progressBar.value = window.scrollY;
+
+  document.addEventListener('scroll', function () {
+        progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
+        progressBar.value = window.scrollY;
+  });
+});
