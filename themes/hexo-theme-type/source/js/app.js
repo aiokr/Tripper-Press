b66c3ecf8012050250248f5a79bd1105ea279358
aiokr <hiaiokr@gmail.com>
2020-02-09 23:11:57 +0800

    新增杂志风格文章页

--- themes/hexo-theme-type/source/js/app.js
+++ themes/hexo-theme-type/source/js/app.js
@@ -67,11 +67,18 @@ $(".itp-post-toc ol").replaceWith(function () {
 
 */
 
-$("img").not('footer img, .drawer-img, .mdui-card-header-avatar').each(function () {
+$("img").not('footer img, .drawer-img, .mdui-card-header-avatar,.itp-mag-content img').each(function () {
   // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
   var element = document.createElement("a");
   $(element).attr("data-fancybox", "gallery");
   $(element).attr("href", $(this).attr("src"));
+  $(this).wrap(element);
+})
+
+$(".itp-mag-content img").each(function () {
+  // $(this).attr("data-fancybox", "gallery"); 直接给img添加data-fancybox会导致点击图片后图片消失
+  var element = document.createElement("div");
+  $(element).attr("class", "itp-mag-img");
   $(this).wrap(element);
 })
 
