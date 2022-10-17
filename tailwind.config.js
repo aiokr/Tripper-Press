773f73030edf5edba56e2ff28e2c5272657b4125
aiokr <hiaiokr@gmail.com>
2022-10-17 19:56:47 +0800

    fix tailwind-css add tailwind cli into theme-ci change style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/**/*.{html,js,ejs,css}","./themes/**/*.{html,js,ejs,css}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        tblack: '#222831',
+      },
+      boxShadow: {
+        'itp': '1px 10px 30px 0 rgb(0 0 0 / 10%)',
+      }
+    }
+  }
 }
