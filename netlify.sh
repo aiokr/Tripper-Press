383c0dd0baedb6a5cb0fac2cfe936aa2751a3158
aiokr <hiaiokr@gmail.com>
2022-11-15 16:31:35 +0800

    修复首次访问评论不生效的问题

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,3 @@
-cp -r themes/hexo-theme-type/README.md source/type-README.md
-npm run tailwind
+npm run tailbuild
 npm run clean
 npm run generate
-cp -r assets/manifest.json public/manifest.json
--- vercel.sh
+++ vercel.sh
@@ -1,4 +1,3 @@
 npm run tailbuild
 npm run clean
 npm run generate
-cp -r assets/manifest.json public/manifest.json
